f3c1b10d62a7aa8c0e26260eed159aabb5925dd4
Rick Cotter <rcotter@gmail.com>
2018-04-07 19:13:29 -0600

    Added axios and fetch game results

--- app/game-results/client.js
+++ app/game-results/client.js
@@ -1,7 +1,35 @@
-// TODO Fetch real results
-//      Promise linting error is fine for now since this is temporary.
-function fetch() {
-  return new Promise(resolve => setTimeout(resolve({ msg: 'Here are your game results' }), 3000));
+const axios = require('axios');
+
+
+const API = 'https://games.api.lottery.com/api/v2.0/results?game=59bc2b6031947b9daf338d32'; // TODO: Move to config
+const ERROR = `Error calling ${API}`;
+
+function logErrors(err) {
+  if (err.response) {
+    // The request was made and the server responded with a status code that falls out of the range of 2xx
+    console.warn(ERROR, err.response.data);
+    console.warn(ERROR, err.response.status);
+    console.warn(ERROR, err.response.headers);
+
+  } else if (err.request) {
+    // The request was made but no response was received. `error.request` is an instance of http.ClientRequest.
+    console.warn(ERROR, err.request);
+
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.warn(ERROR, err.message);
+  }
+
+  console.warn(ERROR, err.config);
+}
+
+async function fetch({ log = true } = {}) {
+  try {
+    return await axios.get(API);
+  } catch (err) {
+    log && logErrors(err);
+    return null;
+  }
 }
 
 module.exports = fetch;
---
+++ test/app/game-results/client.test.js
@@ -0,0 +1,27 @@
+const test = require('ava');
+const td = require('testdouble');
+
+
+const axios = td.replace('axios');
+const GAME_RESULTS_PATH = '../../../app/game-results';
+const fetchGameResults = require(`${GAME_RESULTS_PATH}/client`);
+
+test('Returns Game Results', async t => {
+  td
+    .when(axios.get('https://games.api.lottery.com/api/v2.0/results?game=59bc2b6031947b9daf338d32'))
+    .thenResolve('SOME JSON GAME RESULTS');
+
+  const gameResults = await fetchGameResults();
+
+  t.is(gameResults, 'SOME JSON GAME RESULTS');
+});
+
+test('Returns Null On Error', async t => {
+  td
+    .when(axios.get)
+    .thenReject(new Error('SOMETHING BAD HAPPENED'));
+
+  const gameResults = await fetchGameResults({log: false});
+
+  t.is(gameResults, null);
+});
